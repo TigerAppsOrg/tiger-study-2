337a60730333e6338e271e9f33cae8d9047e2804
joshuamotoaki <dachshunduniverse@gmail.com>
2024-10-03 20:02:40 -0400

    Boilerplate app

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { ServerLoad } from "@sveltejs/kit";
-import { Database } from "bun:sqlite";
-
-export const load: ServerLoad = async () => {
-    const db = new Database(":memory:");
-    const query = db.query("select 'Hello world' as message;");
-    console.log(query.get()); // => { message: "Hello world" }
-};
