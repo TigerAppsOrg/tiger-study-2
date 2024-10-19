18cf8986fb2265930979b73d46cbb7938b63e6f1
Joshua Lau <dachshunduniverse@gmail.com>
2024-10-19 12:43:36 -0400

    Build out schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,8 +1,44 @@
-import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import {
+    text,
+    integer,
+    sqliteTable,
+    primaryKey
+} from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
-    netid: text("netid").notNull().primaryKey(),
+    netid: text("netid").notNull().primaryKey().unique(),
     displayname: text("name").notNull(),
     mail: text("mail").notNull(),
     is_admin: integer("is_admin", { mode: "boolean" }).notNull().default(false)
 });
+
+export const courses = sqliteTable("courses", {
+    id: text("id").notNull().primaryKey().unique(),
+    code: text("code").notNull(),
+    title: text("title").notNull()
+});
+
+export const groups = sqliteTable("groups", {
+    id: text("id").notNull().primaryKey().unique(),
+    name: text("name").notNull(),
+    course_id: text("course_id")
+        .notNull()
+        .references(() => courses.id, {
+            onDelete: "cascade"
+        })
+});
+
+export const group_members = sqliteTable(
+    "group_members",
+    {
+        user_id: text("user_id")
+            .notNull()
+            .references(() => users.netid),
+        group_id: text("group_id")
+            .notNull()
+            .references(() => groups.id)
+    },
+    t => ({
+        pk: primaryKey({ columns: [t.user_id, t.group_id] })
+    })
+);
