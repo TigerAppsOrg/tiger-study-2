d0b0a7bd06da51343e885946e3b58a2afc9ca0bc
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-22 22:50:44 -0500

    Copy dashboard

--- src/lib/client/state.svelte.ts
+++ src/lib/client/state.svelte.ts
@@ -9,8 +9,15 @@ type Course = {
 };
 
 export const joinDialogOpen = $state<Box<boolean>>({ value: false });
+
 export const feedbackDialogOpen = $state<Box<boolean>>({ value: false });
+
 export const courses = $state<Box<Course[]>>({ value: [] });
+
 export const selectedCourse = $state<Box<Course | null>>({
     value: null
+});
+
+export const userGroups = $state<Box<any[]>>({
+    value: []
 });
