f7c8a4b44b6db073fff851e2d0be1e5356ab59f9
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-23 11:43:33 -0500

    Add seed data

--- src/routes/(protected)/admin/+page.server.ts
+++ src/routes/(protected)/admin/+page.server.ts
@@ -2,47 +2,45 @@ import { httpCodes } from "$lib/httpCodes";
 import { CASClient } from "$lib/server/cas";
 import { db } from "$lib/server/db";
 import { users } from "$lib/server/db/schema";
+import { seed } from "$lib/server/seed";
 import { updateCourses } from "$lib/server/updateCourses";
 import { error, type Actions, type ServerLoad } from "@sveltejs/kit";
 import { eq } from "drizzle-orm";
 
-const checkAdmin = async (netid: string) => {
+// Check if a user is an admin, and if not, throw an error
+const adminGuard = async (locals: App.Locals, isLoad: boolean = false) => {
+    const sessionData = locals.session.data;
+    CASClient.check(sessionData);
+
     const user = await db
         .select({
             isAdmin: users.isAdmin
         })
         .from(users)
-        .where(eq(users.netid, netid));
+        .where(eq(users.netid, sessionData.netid));
 
-    return user.length !== 0 && user[0].isAdmin;
+    if (user.length === 0 || !user[0].isAdmin) {
+        const doThis = isLoad ? "view this page" : "perform this action";
+        error(
+            httpCodes.error.forbidden,
+            `You are not authorized to ${doThis}. Please contact an administrator if you believe this to be incorrect.`
+        );
+    }
 };
 
 export const load: ServerLoad = async (req) => {
-    const sessionData = req.locals.session.data;
-    CASClient.check(sessionData);
-
-    const isAdmin = await checkAdmin(sessionData.netid);
-    if (!isAdmin) {
-        error(
-            httpCodes.error.forbidden,
-            "You are not authorized to view this page. Please contact an administrator if you believe this to be incorrect."
-        );
-    }
-
+    await adminGuard(req.locals, true);
     return {};
 };
 
 export const actions: Actions = {
     updateCourses: async ({ locals }) => {
-        const sessionData = locals.session.data;
-        CASClient.check(sessionData);
-        if (!(await checkAdmin(sessionData.netid))) {
-            error(
-                httpCodes.error.forbidden,
-                "You are not perform this action. Please contact an administrator if you believe this to be incorrect."
-            );
-        }
-
+        await adminGuard(locals);
         await updateCourses();
+    },
+
+    seed: async ({ locals }) => {
+        await adminGuard(locals);
+        await seed();
     }
 };
--- src/routes/(protected)/admin/+page.svelte
+++ src/routes/(protected)/admin/+page.svelte
@@ -5,3 +5,7 @@
 <form action="?/updateCourses" method="POST">
     <Button type="submit">Update Courses</Button>
 </form>
+
+<form action="?/seed" method="POST">
+    <Button type="submit">Seed</Button>
+</form>
