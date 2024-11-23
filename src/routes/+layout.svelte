ab02b49b191ac59f1c32d8b3e15f6429ccfefb46
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-23 11:15:25 -0500

    Polling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+    import { invalidateAll } from "$app/navigation";
+    import { Toaster } from "$lib/components/ui/sonner";
     import "@fontsource/lato";
     import "@fontsource/spectral";
+    import { onMount } from "svelte";
     import "../app.css";
-    import { Toaster } from "$lib/components/ui/sonner";
     let { children } = $props();
+
+    // Refresh the page every REFRESH_INTERVAL_MINUTES
+    const REFRESH_INTERVAL_MINUTES = 10;
+    onMount(() => {
+        setInterval(
+            () => {
+                invalidateAll();
+            },
+            REFRESH_INTERVAL_MINUTES * 60 * 1000
+        );
+    });
 </script>
 
 <svelte:head>
