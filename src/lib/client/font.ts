3e00232a758f4e03f642c130c74f77e9b31dec6c
Joshua Lau <dachshunduniverse@gmail.com>
2024-11-23 21:15:18 -0500

    Fix font toggling

--- src/lib/client/font.ts
+++ src/lib/client/font.ts
@@ -1,19 +1,26 @@
-export const loadFont = (fontName: string) => {
+const FONT_MAP = {
+    lato: "Lato, sans-serif",
+    spectral: "Spectral, serif"
+};
+
+export const loadFont = (fontName: keyof typeof FONT_MAP) => {
     switch (fontName) {
         case "spectral":
-            document.documentElement.style.fontFamily = "Spectral";
+            document.documentElement.style.fontFamily = FONT_MAP["spectral"];
             break;
         case "lato":
-            document.documentElement.style.fontFamily = "Lato";
+            document.documentElement.style.fontFamily = FONT_MAP["lato"];
             break;
     }
 };
 
 export const toggleFont = () => {
-    const currentFont = document.documentElement.style.fontFamily;
-    if (currentFont === "Spectral") {
-        document.documentElement.style.fontFamily = "Lato";
+    const currentFont =
+        document.documentElement.style.fontFamily || FONT_MAP["spectral"];
+
+    if (currentFont === FONT_MAP["spectral"]) {
+        document.documentElement.style.fontFamily = FONT_MAP["lato"];
     } else {
-        document.documentElement.style.fontFamily = "Spectral";
+        document.documentElement.style.fontFamily = FONT_MAP["spectral"];
     }
 };
